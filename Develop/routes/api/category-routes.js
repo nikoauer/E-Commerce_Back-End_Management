227980792ddd1cd1aa4a2861030c5bfb54a398bd
Niko Auer <nikoauer26@gmail.com>
2023-08-16 15:40:00 +1000

    added update query to categories

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -36,13 +36,26 @@ router.post('/', async (req, res) => {
     })
     res.status(201).json(newCategory);
   } catch (err) {
-    res.status(500).json(err)
+    res.status(500).json(err);
   }
   // create a new category
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try{
+    const updatedCategory = await Category.update({
+      category_name: req.body.category_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      }
+    })
+    res.status(200).json(updatedCategory) 
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
