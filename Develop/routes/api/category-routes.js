0a47511289e077e2c11126bf1b4bb66fe3197548
Niko Auer <nikoauer26@gmail.com>
2023-08-16 12:28:48 +1000

    category get querys working

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,14 +3,30 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try {
+  const dbData = await Category.findAll({
+    include: [{ model: Product }]
+  });
+  res.status(200).json(dbData);
+} catch (err) {
+  res.status(500).json(err);
+}
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try{
+    const dbData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }]
+    })
+    if(!dbData) {
+    res.status(404).json({ message: 'No Category found with that id!' });
+      return;
+  }
+    res.status(200).json(dbData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
